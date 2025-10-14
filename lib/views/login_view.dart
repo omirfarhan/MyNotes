96319bf0a54f7eff839e381cb034d255d37435bb
omirfarhan <162728245+omirfarhan@users.noreply.github.com>
2025-10-14 22:25:14 +0600

    initall commmit

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -88,7 +88,7 @@ class _LoginViewState extends State<LoginView> {
                           print(credential);
                         } on FirebaseAuthException catch (e){
                           if(e.code=='wrong-password'){
-                            Fluttertoast.showToast(msg: 'Wrong Password ');
+                            Fluttertoast.showToast(msg: 'Wrong Password please right password confirm');
                             print('Wrong password');
                           }
                         }
