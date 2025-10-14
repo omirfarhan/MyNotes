6c009279a6125058fc409de515558a058fc934e1
omirfarhan <162728245+omirfarhan@users.noreply.github.com>
2025-10-14 19:33:11 +0600

    Inital commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # MyNotes
-My Notes is Flutter Application using Firebase
+
+MyNotes is a Flutter application built using Firebase.
+
+## Features
+- Create, edit, and delete notes
+- Firebase Authentication
+- Cloud Firestore integration
+- Clean and simple UI
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+If this is your first Flutter project, here are some helpful resources:
+
+- [Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Flutter Cookbook](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, check out the
+[Flutter online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/views/login_view.dart
@@ -0,0 +1,115 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import '../firebase_options.dart';
+
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+
+
+class _LoginViewState extends State<LoginView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+
+  @override
+  void initState() {
+    _email=TextEditingController();
+    _password=TextEditingController();
+    super.initState();
+  }
+
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+        title: 'flutter demo',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Login'),
+          ),
+
+          body: FutureBuilder(
+            future: Firebase.initializeApp(
+              options: DefaultFirebaseOptions.currentPlatform,
+            ),
+            builder: (context, asyncSnapshot) {
+              switch(asyncSnapshot.connectionState){
+
+                case ConnectionState.done:
+
+                  return Column(
+
+                    children: [
+                      TextField(
+                        controller: _email,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Enter Your Email',
+
+                        ),
+                      ),
+                      TextField(
+                        controller: _password,
+                        obscureText: true,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        decoration:InputDecoration(labelText: 'Enter your password'
+                        ),
+                      ),
+
+                      TextButton(onPressed:() async {
+                        final email=_email.text;
+                        final password=_password.text;
+
+                        try{
+                          final credential= await FirebaseAuth.instance.
+                          signInWithEmailAndPassword(
+                              email: email,
+                              password: password);
+                          print(credential);
+                        } on FirebaseAuthException catch (e){
+                          if(e.code=='wrong-password'){
+                            Fluttertoast.showToast(msg: 'Wrong Password please right password confirm');
+                            print('Wrong password');
+                          }
+                        }
+
+
+                      },
+
+                        child: const Text('Login'),
+
+                      )
+                    ],
+                  );
+                default:
+                  return const Text('This is Loading...');
+              }
+
+
+            },
+          ),
+
+        )
+    );
+  }
+}
